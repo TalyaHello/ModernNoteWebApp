bfad33a30e95571a328bb2c9415e921630bc34f3
Natalya <akva888@mail.ru>
2023-01-10 16:15:33 +0300

    add tests

---
+++ Note.Tests/Common/NotesContextFactory.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+using Notes.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Tests.Common
+{
+    public class NotesContextFactory
+    {
+        public static Guid UserAId = Guid.NewGuid();
+        public static Guid UserBId = Guid.NewGuid();
+
+        public static Guid NoteIdForDelete = Guid.NewGuid();
+        public static Guid NoteIdForUpdate = Guid.NewGuid();
+
+        public static NotesDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<NotesDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            var context = new NotesDbContext(options);
+            context.Database.EnsureCreated();
+            context.Notes.AddRange(
+                new Note
+                {
+                    CreationDate = DateTime.Today,
+                    Details = "Details1",
+                    EditDate = null,
+                    Id = Guid.Parse("2029BD8A-138E-4F7A-B92F-6FE7E7606F23"),
+                    Title = "Title1",
+                    UserId = UserAId
+                },
+                new Note
+                {
+                    CreationDate = DateTime.Today,
+                    Details = "Details2",
+                    EditDate = null,
+                    Id = Guid.Parse("6DD4571F-E756-49BF-B1C2-97CE94F702F3"),
+                    Title = "Title2",
+                    UserId = UserBId
+                },
+                new Note
+                {
+                    CreationDate = DateTime.Today,
+                    Details = "Details3",
+                    EditDate = null,
+                    Id = NoteIdForDelete,
+                    Title = "Title3",
+                    UserId = UserAId
+                },
+                new Note
+                {
+                    CreationDate = DateTime.Today,
+                    Details = "Details4",
+                    EditDate = null,
+                    Id = NoteIdForUpdate,
+                    Title = "Title4",
+                    UserId = UserBId
+                }
+            );
+            context.SaveChanges();
+            return context;
+        }
+
+        public static void Destroy(NotesDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+    }
+}
