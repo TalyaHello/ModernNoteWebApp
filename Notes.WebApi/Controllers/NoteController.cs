45c4507890d8cc6393fa086235838804f3376d21
Natalya <akva888@mail.ru>
2023-01-11 10:16:59 +0300

    add swagger

--- Notes.WebApi/Controllers/NoteController.cs
+++ Notes.WebApi/Controllers/NoteController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Notes.Application.Notes.Commands.CreateNote;
 using Notes.Application.Notes.Commands.DeleteNote;
@@ -12,6 +13,7 @@ using System.Threading.Tasks;
 
 namespace Notes.WebApi.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     public class NoteController : BaseController
     {
@@ -19,8 +21,20 @@ namespace Notes.WebApi.Controllers
 
         public NoteController(IMapper mapper) => _mapper = mapper;
 
+        /// <summary>
+        /// Gets the list of notes
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// GET /note
+        /// </remarks>>
+        /// <returns>Returns NoteListVm</returns>
+        /// <responce code="200">Success</responce>
+        /// <responce code="401">If the user is unauthorized</responce>
         [HttpGet]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<NoteListVm>> GetAll()
         {
             var query = new GetNoteListQuery
@@ -31,8 +45,21 @@ namespace Notes.WebApi.Controllers
             return Ok(vm);
         }
 
+        /// <summary>
+        /// Gets the note by id
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// GET /note/1A742D8F-5457-4691-9ED9-96D257F9EB52
+        /// </remarks>
+        /// <param name="id">Note id (guid)</param>
+        /// <returns>Returns NoteDetailsVm</returns>
+        /// <responce code="200">Success</responce>
+        /// <responce code="401">If the user is unauthorized</responce>
         [HttpGet("{id}")]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<NoteDetailsVm>> Get(Guid id)
         {
             var query = new GetNoteDetailsQuery
@@ -44,8 +71,25 @@ namespace Notes.WebApi.Controllers
             return Ok(vm);
         }
 
+        /// <summary>
+        /// Creates the note
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// POST /note
+        /// {
+        ///     title: "note title",
+        ///     details: "note details"
+        /// }
+        /// </remarks>
+        /// <param name="createNoteDto">CreateNoteDto object</param>
+        /// <returns>Returns id (guid)</returns>
+        /// <responce code="200">Success</responce>
+        /// <responce code="401">If the user is unauthorized</responce>
         [HttpPost]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<Guid>> Create([FromBody] CreateNoteDto createNoteDto)
         {
             var command = _mapper.Map<CreateNoteCommand>(createNoteDto);
@@ -54,8 +98,24 @@ namespace Notes.WebApi.Controllers
             return Ok(noteId);
         }
 
+        /// <summary>
+        /// Updates the note
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// PUT /note
+        /// {
+        ///     title: "updated note title"
+        /// }
+        /// </remarks>
+        /// <param name="updateNoteDto">UpdateNoteDto object</param>
+        /// <returns>Returns NoContent</returns>
+        /// <responce code="200">Success</responce>
+        /// <responce code="401">If the user is unauthorized</responce> 
         [HttpPut]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> Update([FromBody] UpdateNoteDto updateNoteDto)
         {
             var command = _mapper.Map<UpdateNoteCommand>(updateNoteDto);
@@ -64,8 +124,21 @@ namespace Notes.WebApi.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Deletes the note
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// DELETE /note/25090E29-4AE1-4E5B-BF47-2A3BFB340708
+        /// </remarks>
+        /// <param name="id">Note id (guid)</param>
+        /// <returns>Returns NoContent</returns>
+        /// <responce code="200">Success</responce>
+        /// <responce code="401">If the user is unauthorized</responce> 
         [HttpDelete("{id}")]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> Delete(Guid id)
         {
             var command = new DeleteNoteCommand
--- Notes.WebApi/Startup.cs
+++ Notes.WebApi/Startup.cs
@@ -12,6 +12,7 @@ using Notes.Persistence;
 using Notes.WebApi.Middleware;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -54,6 +55,12 @@ namespace Notes.WebApi
                     options.Audience = "NotesWebAPI";
                     options.RequireHttpsMetadata = false;
                 });
+            services.AddSwaggerGen(config =>
+            {
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                config.IncludeXmlComments(xmlPath);
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -62,7 +69,12 @@ namespace Notes.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(config =>
+            {
+                config.RoutePrefix = string.Empty;
+                config.SwaggerEndpoint("swagger/v1/swagger.json", "Notes API");
+            });
             app.UseCustomExceptionHandler();
             app.UseRouting();
             app.UseHttpsRedirection();
